1bdda7fb7858a0e036717a73c0d313e9662c9de6
skj <tlsrbwls999@naver.com>
2021-09-15 19:43:35 +0900

    feat:First_settings

---
+++ src/test/java/com/example/fit_buddy/FitBuddyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fit_buddy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitBuddyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
